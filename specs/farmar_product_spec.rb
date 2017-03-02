7fbc75583050863ee196f02ddca0537250ff15a3
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-03-01 22:49:46 -0800

    write its for product specs

--- specs/farmar_product_spec.rb
+++ specs/farmar_product_spec.rb
@@ -1,15 +1,55 @@
 require_relative 'spec_helper'
 require_relative '../lib/farmar_product.rb'
 
-describe "" do
-  it "" do
+describe "FarMar::Product class" do
+  it "is an instance of FarMar::Product" do
 
   end
 
   describe "initialize" do
   end
 
-  describe "" do
+  describe "self.all" do
+
+    it "" do
+
+    end
+
+  end
+
+  describe "self.find" do
+
+    it "" do
+
+    end
+
+  end
+
+  describe "self.between" do
+
+    it "" do
+
+    end
+
+  end
+
+  describe "vendor" do
+
+    it "" do
+
+    end
+
+  end
+
+  describe "sales" do
+
+    it "" do
+
+    end
+
+  end
+
+  describe "number_of_sales" do
 
     it "" do
 
